0208da5dcdc851c1d60623a627f0d535f4a0c1e4
Tamarin Vyacheslav <slavabarsuk@ya.ru>
2018-07-23 12:28:53 +0300

    redirect / to /missions/menu/

--- translation_project/urls.py
+++ translation_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('missions/', include('missions.urls')),
+    path('', RedirectView(url='/missions/base/')),
 ]
