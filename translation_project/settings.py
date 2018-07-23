bcb84d8ea8b18b9d5c5fac757cd7fc5abaacc1f2
Tamarin Vyacheslav <slavabarsuk@ya.ru>
2018-07-23 11:35:05 +0300

    change settings.py

--- translation_project/settings.py
+++ translation_project/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'translation_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+        'NAME': os.path.join(BASE_DIR, 'postgresql.db'),
     }
 }
 if not DEBUG:
