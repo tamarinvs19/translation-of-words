dae3b6f0c43f5ccc8e58f92ad2d7cfdb6d9f14ce
Tamarin Vyacheslav <slavabarsuk@ya.ru>
2018-08-31 16:40:30 +0300

    setting select dict4

--- missions/forms.py
+++ missions/forms.py
@@ -4,9 +4,9 @@ from .models import Mission
 
 
 class MissionForm(forms.Form):
-    DICTIONARY = ((1, 'workbook',), (2, 'IT'))
+    DICTIONARY = (('workbook', 'workbook',), ('IT', 'IT'))
     lang = forms.CharField(initial='ru / en', label='Language', max_length=2)
     count_of_words = forms.IntegerField(initial='20', label='Count of words')
-    dictionary = forms.ChoiceField(widget=forms.Select, choices=DICTIONARY)
+    dictionary = forms.Select(choices=DICTIONARY)#forms.ChoiceField(widget=forms.Select, choices=DICTIONARY)
     #mode = forms.CharField(label='Mode')
 
