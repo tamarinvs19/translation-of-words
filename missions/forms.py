4a61338649cc65c69cbb0bcba6427c31215981e7
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2019-09-11 20:32:33 +0300

    add university dict to form

--- missions/forms.py
+++ missions/forms.py
@@ -4,7 +4,7 @@ from .models import Mission
 
 
 class MissionForm(forms.Form):
-    DICTIONARY = (('IT', 'IT'), ('workbook', 'workbook',))
+    DICTIONARY = (('University', 'university'), ('IT', 'IT'), ('workbook', 'workbook',))
     lang = forms.CharField(initial='ru / en', label='Language', max_length=2)
     count_of_words = forms.IntegerField(initial='20', label='Count of words')
     dictionary = forms.ChoiceField(widget=forms.Select, choices=DICTIONARY, label='Dictionary')
