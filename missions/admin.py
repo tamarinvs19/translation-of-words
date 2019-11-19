ca1b40a74622aef887a7d51baa47cdd496e92b86
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2019-11-19 19:59:56 +0300

    add admin

--- missions/admin.py
+++ missions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .views import Mission
 
-# Register your models here.
+class MissionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Words', {'fields': ['count_of_words', 'words']}),
+        ('Language', {'fields': ['lang', 'dictionary']}),
+        ('Result', {'fields': ['result']}),
+        ('Time', {'fields': ['start_time']}),
+    ]
+    list_display = ('lang', 'count_of_words', 'dictionary', 'start_time')
+    list_filter = ['start_time']
+
+admin.site.register(Mission)
